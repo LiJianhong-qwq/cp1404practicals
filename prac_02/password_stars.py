dc8d8deb995c339523c53465295e04f444a69d6c
LiJianhong-qwq <hong2941187667@gmail.com>
2023-03-20 10:56:23 +0800

    Separate the program into 2 functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,19 @@
-password = input("Password: ")
-while len(password) < 6:
-    print("Password too short")
-    password = input("Password: ")
+def main():
+    password = get_password()
+    make_asterisks(password)
 
-print(f"Your password is: {'*' * len(password)}")
+
+def make_asterisks(password):
+    print(f"Your password is: {'*' * len(password)}")
+
+
+def get_password():
+    password = input("Password: ")
+    while len(password) < 6:
+        print("Password too short")
+        password = input("Password: ")
+    return password
+
+
+main()
 
