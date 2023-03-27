21908aa71f564e5c616591e0f2cea2979fb13ea3
LiJianhong-qwq <hong2941187667@gmail.com>
2023-03-27 10:16:19 +0800

    Answer the question and add a code

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+# Answer: Line 1 is an integer between 5 and 20. The smallest number I could see is 5 and the largest is 20
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+# Answer:  It can be 3, 5, 7 and 9. The smallest is 3 and the largest is 9. It could not produce a 4.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+# Answer: A string of decimals. The smallest is 2.5 and the largest is 5.5.
+
+import random
+print(f"{random.uniform(1, 100)}")
+
+
+
+
