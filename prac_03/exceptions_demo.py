c97cb5950881634c5a06872b83f885870601e5b8
LiJianhong-qwq <hong2941187667@gmail.com>
2023-03-27 10:36:46 +0800

    Answer the question and change the program.

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Could not divide by 0")
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+print("Finished.")
+
+# 1, When you need to enter a specified type and enter another type, this error will occur.
+# 2, Appears when a value is divided by 0.
+# 3，When 0 appears, print a wrong message.
+
+
+
+
