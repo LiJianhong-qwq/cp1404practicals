92bca0245fbb3642e156e33ba67d51f450fead0f
LiJianhong-qwq <hong2941187667@gmail.com>
2023-05-14 19:03:50 +0800

    doing all the part

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class DynamicLabelsApp(App):
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        return self.root
+
+
+DynamicLabelsApp().run()
+
+
+
+
+
+
+
+
+
+
