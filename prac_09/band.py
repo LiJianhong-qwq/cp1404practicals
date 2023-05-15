1ab66fe82bf0e28865301fcfb7a918b2f279d869
LiJianhong-qwq <hong2941187667@gmail.com>
2023-05-15 11:52:43 +0800

    make a general framework

---
+++ prac_09/band.py
@@ -0,0 +1,15 @@
+class Band:
+
+    def __init__(self, name):
+        self.name = name
+        self.bands = []
+
+    def __str__(self):
+        return
+
+    def add(self, band):
+        self.bands.append(band)
+
+    def play(self):
+        pass
+
