67561159514c445afeb91a43b8c1fed6e0f390a1
LiJianhong-qwq <hong2941187667@gmail.com>
2023-05-18 13:16:05 +0800

    finish the prac

--- prac_09/band.py
+++ prac_09/band.py
@@ -1,15 +1,21 @@
 class Band:
+    bands = []
 
     def __init__(self, name):
         self.name = name
-        self.bands = []
 
     def __str__(self):
-        return
+        band_list = ', '.join(str(i) for i in self.bands)
+        return f"{self.name}({band_list})"
 
     def add(self, band):
         self.bands.append(band)
 
     def play(self):
-        pass
+        for band in self.bands:
+            if not vars(band)['instruments']:
+                print(f"{vars(band)['name']} needs an instrument!")
+            else:
+                print(f"{vars(band)['name']} is playing: {vars(band)['instruments'][0]}")
+        return ""
 
