e5e1684c3467cf53d6a6697cf497cdd6a4834c15
LiJianhong-qwq <hong2941187667@gmail.com>
2023-05-19 17:37:56 +0800

    finish the work and add some new function

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -11,25 +11,44 @@ def main():
     print("Let's drive!")
     print("q)uit, c)hoose taxi, d)rive")
     user_choice = input(">>> ").upper()
-    while user_choice != "Q" and user_choice != "C" and user_choice != "D" :
-        print("Invalid option")
-        print(f"Bill to date: ${total_bill:.2f}")
-        print("q)uit, c)hoose taxi, d)rive")
-        user_choice = input(">>> ").upper()
+    user_choice = check_input(total_bill, user_choice)
 
     while user_choice != "Q":
         if user_choice == "C":
             current_taxi = choose_taxi()
 
         if user_choice == "D":
-            if current_taxi is None:
-                print("You need to choose a taxi before you can drive")
-            else:
-                pass
+            total_bill = drive_car(current_taxi, total_bill)
 
         print(f"Bill to date: ${total_bill:.2f}")
         print("q)uit, c)hoose taxi, d)rive")
         user_choice = input(">>> ").upper()
+        user_choice = check_input(total_bill, user_choice)
+
+    print(f"Total trip cost: ${total_bill}")
+    print("Taxis are now:")
+    for i, j in zip(taxis, range(0, len(taxis))):
+        print(f"{j} - {i}")
+
+
+def drive_car(current_taxi, total_bill) :
+    if current_taxi is None :
+        print("You need to choose a taxi before you can drive")
+    else :
+        drive_distance = float(input("Drive how far? "))
+        current_taxi.drive(drive_distance)
+        print(f"Your {current_taxi.name} trip cost you ${float(current_taxi.get_fare()):.2f}")
+        total_bill += float(current_taxi.get_fare())
+    return total_bill
+
+
+def check_input(total_bill, user_choice) :
+    while user_choice != "Q" and user_choice != "C" and user_choice != "D" :
+        print("Invalid option")
+        print(f"Bill to date: ${total_bill:.2f}")
+        print("q)uit, c)hoose taxi, d)rive")
+        user_choice = input(">>> ").upper()
+    return user_choice
 
 
 def choose_taxi():
